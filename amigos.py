d4b665f41ab807eea7d912feb146f6cca85980e4
Martin <marttinguerreroo@gmail.com>
2025-08-31 14:13:29 -0300

    Arreglo para que se impriman los numeros perfectos tambien

--- amigos.py
+++ amigos.py
@@ -3,9 +3,6 @@ import time
 def amigos(MAX: int):
 
     t1 = time.time()
-    if MAX < 2:
-        print(0.0)
-        return
 
     suma_divisores_propios = [0] * (MAX + 1)
 
@@ -13,9 +10,9 @@ def amigos(MAX: int):
         for m in range(2 * d, MAX + 1, d):
             suma_divisores_propios[m] += d
 
-    for i in range(2, MAX + 1):
+    for i in range(0, MAX + 1):
         s = suma_divisores_propios[i]
-        if s > i and s <= MAX and suma_divisores_propios[s] == i:
+        if s >= i and s <= MAX and suma_divisores_propios[s] == i:
             print(i, s)
 
     t2 = time.time()
